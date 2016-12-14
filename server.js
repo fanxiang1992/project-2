87f8813e3277cd5867d947b07472a92d4e27a453
Xiang Fan <xiangfan1992@gmail.com>
2016-12-14 00:43:35 -0500

    test again

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+// app.set('port', (process.env.PORT || 5000));
 
 // app.use(express.static(__dirname + '/public'));
 
@@ -35,8 +35,8 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Node app is running on port', this.address().port);
 });
 
 //var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
