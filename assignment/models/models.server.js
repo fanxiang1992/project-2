dd2302abb328ed50ec366b4dd5b821e4566911dc
Xiang Fan <xiangfan1992@gmail.com>
2016-12-14 00:12:38 -0500

    test heroku

--- assignment/models/models.server.js
+++ assignment/models/models.server.js
@@ -1,13 +1,14 @@
 module.exports = function() {
 
-    var connectionString = 'mongodb://localhost/fan-xiang-webdev';
-    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-            process.env.OPENSHIFT_APP_NAME;
-    }
+    // var connectionString = 'mongodb://localhost/fan-xiang-webdev';
+    var connectionString = 'mongodb://heroku_smq3mkz6:akstc254qrotv2pg6188sutrl6@ds133378.mlab.com:33378/heroku_smq3mkz6'
+    // if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    //     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    //         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+    //         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+    //         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+    //         process.env.OPENSHIFT_APP_NAME;
+    // }
 
     var mongoose = require('mongoose');
     mongoose.connect(connectionString);
