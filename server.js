31dc39703047cd8ba25ac6d23682985b59b7ebdc
Xiang Fan <xiangfan1992@gmail.com>
2016-12-14 00:36:55 -0500

    test again

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var app = express();
 
-// app.set('port', (process.env.PORT || 5000));
-app.set('port', 5000);
+app.set('port', (process.env.PORT || 5000));
 
 // app.use(express.static(__dirname + '/public'));
 
@@ -17,14 +16,15 @@ app.set('port', 5000);
 // app.listen(app.get('port'), function() {
 //   console.log('Node app is running on port', app.get('port'));
 // });
-
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-// app.set('port', (process.env.PORT || 3000));
-
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
+
+// var bodyParser = require('body-parser');
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.set('port', (process.env.PORT || 3000));
+
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
@@ -34,8 +34,6 @@ require ("./assignment/app.js")(app);
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
-
-
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
